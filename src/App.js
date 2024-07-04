f76a04ad01189e00ec93b246ef2cc0a85fe7633d
BAN1337 <joaocleber.jc10@gmail.com>
2024-07-04 14:00:05 -0300

    Atualização de códigos - Aula 356

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.scss";
+import { Notification, Section } from 'rbx';
+  
+const App = () => (
+  <div>
+    <Section>
+      <Notification color="success">
+        Javascript Notes!
+      </Notification>
+    </Section>
+  </div>
+)
+  
+export default App;
